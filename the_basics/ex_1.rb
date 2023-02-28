0bbc01c66fa804b5919605963e9f5fe36b341f7e
Jorje Hernandez <hernandez4757@gmail.com>
2023-02-27 22:57:04 -0700

    first three exercises in the basics chapter

---
+++ the_basics/ex_1.rb
@@ -0,0 +1,13 @@
+# Name prompt - advanced
+puts "What is your first name?"
+first = gets.chomp.capitalize
+puts "What is your last name?"
+last = gets.chomp.capitalize
+
+puts first + " " + last
+
+# First + Last concat
+first = "Jorje"
+last = "Hernandez"
+
+puts "#{first} #{last}"
---
+++ the_basics/ex_2.rb
@@ -0,0 +1,6 @@
+puts 1234 / 1000
+puts 1234 % 1000 / 100
+puts 1234 % 1000 % 100 / 10
+puts 1234 % 1000 % 100 % 10
+
+puts "Using modulo and division to retrieve specific digits from a 4 digit number (1234)."
