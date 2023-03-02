8bb5debc9526a3cdc9a38bee0d0da218508dc578
Jorje Hernandez <hernandez4757@gmail.com>
2023-03-01 21:46:09 -0700

    added first+last, new lines, removed some

--- variables/name.rb
+++ variables/name.rb
@@ -2,9 +2,12 @@ def greeter(n)
   puts "Hello #{n}, it's so nice to meet you."
 end
 
-puts "Hello and welcome. What is your name?"
-name = gets.chomp
+puts "Hello and welcome. What is your first name?"
+first_name = gets.chomp.capitalize
+puts "and what is your last name?"
+last_name = gets.chomp.capitalize
+full_name = first_name + ' ' + last_name
 
-greeter(name)
+greeter(full_name)
 
-10.times { puts name }
+10.times { puts full_name }
